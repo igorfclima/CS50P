227bdcb3abd29f7cad6ac5aca4c345d3b2abab33
Igor <igrfclima@gmail.com>
2025-07-09 13:19:36 -0300

    feat: comparables and condicionals

---
+++ Week 2/conditionals.py
@@ -0,0 +1,9 @@
+x = int(input("Tell me x: "))
+y = int(input("Tell me y: "))
+
+if x > y:
+    print("X are greater than y.")
+elif x < y:
+    print("Y are grater than x.")
+else:
+    print("X equals Y.")
