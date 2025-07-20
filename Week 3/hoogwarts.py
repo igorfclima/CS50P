e273961c6db38f3ab4562441ddfd5ba6623c081e
Igor Fernandes <igrfclima@gmail.com>
2025-07-19 21:22:32 -0300

    error

--- Week 3/hoogwarts.py
+++ Week 3/hoogwarts.py
@@ -1,7 +1,7 @@
 students = [
     {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
     {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
-    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier "},
     {"name": "Draco", "house": "Slytherin", "patronus": None}
 ]
 
