b56b9eec4e8b864580c9d833beca14b23e143b56
Igor Fernandes <igrfclima@gmail.com>
2025-07-21 10:35:44 -0300

    add: groceries

---
+++ Week 4/Problems sets/grocery.py
@@ -0,0 +1,14 @@
+def groceries():
+    cart = {}
+
+    while True:
+            try:
+                item = input().upper().strip()
+
+                cart[item] = cart.get(item, 0) + 1
+            except EOFError:
+                break
+    sorted_items = sorted(cart.items())
+    for fruits,quant in sorted_items.items():
+        print(f"{quant} {fruits}")
+groceries()
