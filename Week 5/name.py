54fa021a8a35fe3b333ebecd2f76d2152d3e88c9
Igor Fernandes <igrfclima@gmail.com>
2025-07-24 11:30:15 -0300

    add: slice

--- Week 5/name.py
+++ Week 5/name.py
@@ -2,8 +2,6 @@ import sys
 
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
-if len(sys.argv) > 2:
-    sys.exit("Too many arguments")
 
-for arg in sys.argv:
+for arg in sys.argv[1:]:
     print("hello, my name is", arg)
