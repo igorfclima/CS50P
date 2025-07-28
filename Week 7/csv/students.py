9f6b793303fa1ae0efa5f23340b63237f627a573
Igor Fernandes <igrfclima@gmail.com>
2025-07-28 11:05:42 -0300

    feat: sorting and key

--- Week 7/csv/students.py
+++ Week 7/csv/students.py
@@ -1,4 +1,14 @@
+students = []
+
 with open("Week 7/csv/names.csv") as file:
-    for line in sorted(file):
+    for line in file:
         name, house = line.rstrip().split(",")
-        print(f"{name} is in {house}")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+def get_name(student):
+    return student['name']
+
+
+for student in sorted(students, key=get_name):
+    print(f"{student['name']} is in {student['house']}")
